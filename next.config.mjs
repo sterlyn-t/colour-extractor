2e10d3df673c38794da25c1987793d6b11136b50
Sterlyn <sterlyntang31@gmail.com>
2024-11-05 14:23:23 -0500

    Chore: breaking chromium test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  experimental: {
+    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+  },
 };
 
 export default nextConfig;
