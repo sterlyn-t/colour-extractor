757b5cbd46e8826984a9bf6a8f14d745a2c0f0d4
Sterlyn <sterlyntang31@gmail.com>
2024-11-05 13:11:16 -0500

    Chore: testing chromium on vercel

--- app/api/screenshot/route.ts
+++ app/api/screenshot/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import puppeteer from "puppeteer";
 import Vibrant from "node-vibrant";
+import chromium from "chrome-aws-lambda";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl;
@@ -11,7 +12,14 @@ export async function GET(req: NextRequest) {
   }
 
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await chromium.puppeteer.launch({
+      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+    // const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle2" });
 
@@ -27,10 +35,10 @@ export async function GET(req: NextRequest) {
     }
 
     // Convert the Uint8Array to Buffer if needed
-    const buffer = Buffer.from(screenshotBuffer);
+    // const buffer = Buffer.from(screenshotBuffer);
 
     // Use Vibrant to extract colors directly from the screenshot buffer
-    const palette = await Vibrant.from(buffer).getPalette();
+    const palette = await Vibrant.from(screenshotBuffer).getPalette();
 
     // Map palette to RGB color values
     const colors = Object.values(palette).map(
